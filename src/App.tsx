1363d47a732efb19ee0bf539306eaff5ce204d59
mStagnaro17 <michele.stagnaro@gmail.com>
2025-10-21 22:21:49 -0500

    Subiendo proyecto limpio

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-black text-white text-3xl font-bold">
+      Tailwind funciona 🚀
+    </div>
+  )
+}
+
+export default App
